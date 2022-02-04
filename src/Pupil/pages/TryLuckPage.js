6d203d81ae30792a693523bc4a04e4635422bd78
Jiri <kalendar.skolavrchlabi@gmail.com>
2022-02-04 19:07:03 +0100

    try luck park 1

--- src/Pupil/pages/TryLuckPage.js
+++ src/Pupil/pages/TryLuckPage.js
@@ -1,16 +1,53 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
+import { useHttp } from "../../hooks/http-hook";
 
 const TryLuckPage = () => {
-    const [canRoll, setCanRoll] = useState(false);
-    useState(() => {
-        fetchCanRoll()
+  const auth = useContext(AuthContext);
+  const { sendRequest } = useHttp();
+  const [canRoll, setCanRoll] = useState(false);
+  const [reward, setReward] = useState();
 
-        setCanRoll(true)
-    }, [auth.token])
+  // Finds if pupil can roll (so if he hasn'n roll in last 24 hours)
+  useState(() => {
+    const fetchLastRoll = async () => {
+      try {
+        let responseData = await sendRequest(
+          `${process.env.REACT_APP_BACKEND_URL}/api/pupil/can-roll/${auth.userId}`,
+          "GET",
+          null,
+          {
+            "content-type": "application/json",
+            Authorization: "Bearer " + auth.token,
+          }
+        );
+        setCanRoll(responseData.canRoll);
+      } catch (err) {}
+    };
+    fetchLastRoll();
+  }, [auth.token, auth.userId]);
+
+  const postRoll = async () => {
+    try {
+      await sendRequest(
+        `${process.env.REACT_APP_BACKEND_URL}/api/pupil/roll`,
+        "POST",
+        null,
+        {
+          "content-type": "application/json",
+          Authorization: "Bearer " + auth.token,
+        }
+      );
+      setCanRoll(false)
+    } catch (err) {}
+  };
+
   return (
     <div className="custom-page--wrapper">
       <h1 className="custom-page--title">Zkus štěstí ;)</h1>
-      
+      <button className="small-button button-save" disabled={!canRoll} onClick={postRoll}>
+        Zkusit štěstí
+      </button>
     </div>
   );
 };
